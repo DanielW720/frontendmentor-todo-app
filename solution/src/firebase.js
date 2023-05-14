f17303389c41ebae5da4f1c36b7eb3668cd212e0
DW Desktop <westerlund.da@gmail.com>
2023-05-14 17:05:38 +0200

    firebase init with hosting

---
+++ solution/src/firebase.js
@@ -0,0 +1,19 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyB-Vgk3Imbnf9GSOiE2G3-doo0zoZJJoTw",
+//   authDomain: "frontendmentor-todo-5b99b.firebaseapp.com",
+//   projectId: "frontendmentor-todo-5b99b",
+//   storageBucket: "frontendmentor-todo-5b99b.appspot.com",
+//   messagingSenderId: "572024372701",
+//   appId: "1:572024372701:web:fe0d8bda01f3d00ac60a34",
+//   measurementId: "G-KXSWB2MGS0",
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
